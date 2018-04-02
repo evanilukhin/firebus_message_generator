28294082d3d15c7fc8b6a9475ef1c405482b6255
Ivan Ilyukhin <evanilukhin@gmail.com>
2018-04-02 08:40:17 +0300

    Init commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Firebus Message Generator
+
+## Description
+
+*FireBus Message Generator* is part of [FireBus](https://github.com/evanilukhin/firebus) ecosystem. It creates a JSON-messages and puts into a Kafka topic.
+
+## Message Structure
+
+uuid - identifier of the messages group;
+item - unique number of the message in the group;
+created_at - the timestamp is formatted as "15:12:12.124123";
+
+## Usage
+
+``bash
+
+``
+
+## Dependencies
---
+++ generator.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/satori/go.uuid"
+	"time"
+)
+
+//Message - message sended to topic
+type Message struct {
+	Item      int    `json:"item"`
+	UUID      string `json:"uuid"`
+	CreatedAt string `json:"created_at"`
+}
+
+var topic = "virgin_messages"
+
+func main() {
+	kafkaBrokerHost := flag.String("kafka", "0.0.0.0:9092", "Kafka broker address. Default: 0.0.0.0:9092")
+	countMessages := flag.Int("count", 100, "Count messages in a group. Default: 100")
+
+	flag.Parse()
+
+	kafkaProducer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": *kafkaBrokerHost, "go.delivery.reports": false})
+	if err != nil {
+		panic(err)
+	}
+
+	uuid := uuid.Must(uuid.NewV4()).String()
+	fmt.Println("Start generating")
+	for i := 0; i < *countMessages; i++ {
+		message := Message{
+			UUID:      uuid,
+			Item:      i,
+			CreatedAt: time.Now().UTC().Format("15:04:05.999999"),
+		}
+		marshalledMessage, _ := json.Marshal(message)
+
+		kafkaProducer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          marshalledMessage,
+		}, nil)
+		if i%1000000 == 0 {
+			fmt.Printf("Sended %d messages\n", i)
+		}
+	}
+	kafkaProducer.Flush(30000)
+}
